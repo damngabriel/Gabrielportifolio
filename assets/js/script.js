1debfb7a22685eb34db36bcd6724f0ff6e660017
damngabriel <gabrielcamargo0202@gmail.com>
2024-07-29 18:48:19 +0000

    .

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//faz aparecer apos clicar no hamburguer
+document.querySelector('.menu-opener').addEventListener('click', ()=>{
+    let nav=document.querySelector('header nav');
+    nav.classList.toggle('opened');
+});
